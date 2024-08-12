22c86174ef780006e5a8ebe14ac3c8d3019cfa96
shadrach-tayo <shadrachtemitayo@gmail.com>
2024-08-12 08:31:32 +0100

    codecrafters submit [skip ci]

--- src/command/stream/xadd.rs
+++ src/command/stream/xadd.rs
@@ -1,4 +1,7 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    time::{SystemTime, UNIX_EPOCH},
+};
 
 use bytes::Bytes;
 
@@ -43,7 +46,7 @@ impl XAdd {
         Ok(XAdd {
             key,
             fields: pairs,
-            id: None, // Some((stream_id_split[0], stream_id_split[1])),
+            id: None,
             stream_id: Some(stream_id),
         })
     }
@@ -54,18 +57,64 @@ impl XAdd {
             .clone()
             .unwrap_or("".to_string())
             .split('-')
-            .map(|char| char.parse().unwrap())
-            .collect::<Vec<u64>>();
+            .map(|char| char.to_string())
+            .collect::<Vec<String>>();
 
         if ids.len() > 2 {
             return false;
         }
 
-        if ids[1] < 1 {
+        if ids.len() == 2 {
+            if ids[1] == "0" {
+                return ids[0] != "0";
+            }
+        }
+
+        if ids.len() == 1 && ids[0] != "*" {
             return false;
         }
 
         return true;
+    }
+
+    fn get_stream_id(&self, next_sequence_id: u64) -> (u64, u64) {
+        let ids = self
+            .stream_id
+            .clone()
+            .unwrap()
+            .split('-')
+            .map(|char| char.to_string())
+            .collect::<Vec<String>>();
+
+        let now = {
+            let start = SystemTime::now();
+            let since_the_epoch = start
+                .duration_since(UNIX_EPOCH)
+                .expect("Time went backwards");
+            since_the_epoch.as_secs() * 1000 + since_the_epoch.subsec_nanos() as u64 / 1_000_000
+        };
+
+        println!("Now: {now}");
+
+        let millisec = ids
+            .get(0)
+            .map(|t| if t == "*" { now } else { t.parse().unwrap() })
+            .or(Some(now))
+            .unwrap();
+
+        let sequence_id = ids
+            .get(1)
+            .map(|t| {
+                if t == "*" {
+                    next_sequence_id
+                } else {
+                    t.parse().unwrap()
+                }
+            })
+            .or(Some(next_sequence_id))
+            .unwrap();
+
+        (millisec, sequence_id)
     }
 
     /// Apply the stream command and write to the Tcp connection stream
@@ -75,19 +124,6 @@ impl XAdd {
                 "ERR The ID specified in XADD must be greater than 0-0".to_string(),
             )));
         }
-
-        let id = self
-            .stream_id
-            .clone()
-            .unwrap()
-            .split('-')
-            .map(|char| char.parse().unwrap())
-            .collect::<Vec<u64>>();
-
-        let new_stream = StreamData {
-            id: (id[0], id[1]),
-            pairs: self.fields,
-        };
 
         let prev_stream = db.get(&self.key);
 
@@ -100,12 +136,61 @@ impl XAdd {
             vec![]
         };
 
+        let next_sequence_id = {
+            let ids = self
+                .stream_id
+                .clone()
+                .unwrap_or("".to_string())
+                .split('-')
+                // .map(|char| char.parse().unwrap())
+                .map(|char| char.to_string())
+                .collect::<Vec<String>>();
+
+            if ids[0] == "*" {
+                if ids.len() == 1 {
+                    0
+                } else {
+                    1
+                }
+            } else {
+                let time_part: u64 = ids[0].parse().unwrap();
+                let time_match = streams
+                    .iter()
+                    .rev()
+                    .find(|stream| stream.id.0 == time_part)
+                    .map(|stream| stream.id.1);
+
+                if time_match.is_some() {
+                    println!("Match found: {:?}", time_match);
+                    time_match.unwrap() + 1
+                } else {
+                    if ids[0] == "0" {
+                        1
+                    } else {
+                        0
+                    }
+                }
+            }
+        };
+
+        println!("Next Sequence: {next_sequence_id}");
+
+        let stream_id = self.get_stream_id(next_sequence_id);
+
+        let next_stream_id = format!("{}-{}", stream_id.0, stream_id.1);
+
+        let new_stream = StreamData {
+            id: stream_id,
+            pairs: self.fields,
+        };
+
         // The ID should be greater than the ID of the last entry in the stream.
         // The millisecondsTime part of the ID should be greater than or equal to the millisecondsTime of the last entry.
         // If the millisecondsTime part of the ID is equal to the millisecondsTime of the last entry, the sequenceNumber part of the ID should be greater than the sequenceNumber of the last entry.
         // If the stream is empty, the ID should be greater than 0-0
         for stream in streams.iter() {
             if stream.id >= new_stream.id {
+                println!("Prev Stream: {:?}, Stream: {:?}", stream.id, new_stream.id);
                 return Ok(Some(RESP::Error(
                         "ERR The ID specified in XADD is equal or smaller than the target stream top item".to_string(),
                     )));
@@ -121,7 +206,7 @@ impl XAdd {
 
         db.set(self.key, value, None);
 
-        Ok(Some(RESP::Simple(self.stream_id.unwrap())))
+        Ok(Some(RESP::Simple(next_stream_id)))
     }
 }
 
