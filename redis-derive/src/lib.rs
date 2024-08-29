ac800c3a3596ec6f1bddffe90d0ca37c1603ade9
shadrach-tayo <shadrachtemitayo@gmail.com>
2024-08-29 00:58:37 -0500

    chore: clean up

--- redis-derive/src/lib.rs
+++ redis-derive/src/lib.rs
@@ -9,14 +9,20 @@ use proc_macro2::Span;
 
 #[proc_macro]
 pub fn gen_cursor_util(input: TokenStream) -> TokenStream {
-    println!("{:?}", input);
-
     // parse the string representation
     let name = input.to_string();
-    let fn_type_str: Vec<&str> = name.split("_").collect();
-    let type_ident: syn::LitStr = syn::LitStr::new(fn_type_str[1], Span::call_site());
-    let return_type = syn::Ident::new(fn_type_str[1], Span::call_site());
+
+    // convert name string to Identifier
     let fn_name = syn::Ident::new(name.as_str(), Span::call_site());
+
+    // extract string format for the return type
+    let fn_type_str: Vec<&str> = name.split("_").collect();
+    // convert vec['get', 'i8'] to LitStr('i8')
+    let type_ident: syn::LitStr = syn::LitStr::new(fn_type_str[1], Span::call_site());
+
+    // extract the util return type from the name string
+    // convert vec['get', 'i8'] to Ident('i8')
+    let return_type = syn::Ident::new(fn_type_str[1], Span::call_site());
 
     let expanded = quote! {
         fn #fn_name(src: &mut Cursor<&[u8]>) -> Result<#return_type> {
